2e37cd6b0775ee5dbfc35e503dfc480504774b00
Austin-Salameh <61394452+Austin-Salameh@users.noreply.github.com>
2021-07-01 15:57:45 -0500

    Add files via upload

--- web_dev/main.go
+++ web_dev/main.go
@@ -6,7 +6,10 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 type Sitemap struct {
 	Locations []string `xml:"sitemap>loc"`
@@ -30,24 +33,55 @@ type NewsAggPage struct {
 	News  map[string]NewsMap
 }
 
+func newsRoutine(c chan News, Location string) {
+	defer wg.Done()
+	var n News
+	resp, _ := http.Get(Location)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &n)
+	resp.Body.Close()
+	c <- n
+}
+
+func queueWatcher(c chan News) map[string]NewsMap {
+	news_map := make(map[string]NewsMap)
+
+	for elem := range c {
+		for i, _ := range elem.Keywords {
+			news_map[elem.Titles[i]] = NewsMap{elem.Keywords[i], elem.Locations[i], elem.Publication_Dates[i]}
+		}
+	}
+
+	return news_map
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	var s Sitemap
-	var n News
+
 	resp, _ := http.Get("https://www.cbssports.com/index-sitemap.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	xml.Unmarshal(bytes, &s)
-	news_map := make(map[string]NewsMap)
-	// fmt.Println(s.Locations)
+	var news_map map[string]NewsMap
+	resp.Body.Close()
+
+	queue := make(chan News, 30)
+	processingFinished := make(chan bool)
+
+	go func() {
+		news_map = queueWatcher(queue)
+		processingFinished <- true
+	}()
 
 	for _, Location := range s.Locations {
-		resp, _ := http.Get(Location)
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes, &n)
-
-		for i, _ := range n.Keywords {
-			news_map[n.Titles[i]] = NewsMap{n.Keywords[i], n.Locations[i], n.Publication_Dates[i]}
-		}
+		wg.Add(1)
+		go newsRoutine(queue, Location)
 	}
+
+	wg.Wait()
+	close(queue)
+	<-processingFinished
+
+	fmt.Println(news_map)
 
 	p := NewsAggPage{Title: "Sports News Aggregator", News: news_map}
 	t, err := template.ParseFiles("displayNews.html")
